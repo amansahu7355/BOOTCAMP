c067b53cf7beb3a42f109437c47346c0c3a009b4
Aman Sahu <rajaman9651@gmail.com>
2026-06-08 03:09:30 +0530

    D3

---
+++ DAY_3/StringCompression.java
@@ -0,0 +1,24 @@
+public class StringCompression {
+    public static void main(String[] args){
+        String str = "aaaabbbccd";
+
+        StringBuilder result = new StringBuilder();
+        int count = 1;
+
+        for(int i = 1 ; i<str.length();i++){
+            if(str.charAt(i) == str.charAt(i - 1)){
+                count++;
+            }
+            else{
+                result.append(str.charAt(i-1));
+                result.append(count);
+                count = 1;
+            }
+        }
+        result.append(str.charAt(str.length() - 1));
+        result.append(count);
+
+        System.out.println(result);
+    }
+    
+}
